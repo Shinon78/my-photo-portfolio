94823cc52c056521dc1ba5a4558998610ce3063b
Shinon <tf23stamada@ec.usp.ac.jp>
2026-04-20 15:25:38 +0900

    Fix PostAdmin definition in admin.py

--- photos/admin.py
+++ photos/admin.py
@@ -2,5 +2,10 @@ from django.contrib import admin
 from django_summernote.admin import SummernoteModelAdmin
 from .models import PhotoPost, Post
 
+# ① まずここで「PostAdmin」というサマーノート用の設定を作ります
+class PostAdmin(SummernoteModelAdmin):
+    summernote_fields = ('content',)
+
+# ② 設定を作り終わってから、最後に登録します（順番が大事です！）
 admin.site.register(PhotoPost)
 admin.site.register(Post, PostAdmin)
