e64004d235fd5397778b19282a838851c46828d9
Shinon <tf23stamada@ec.usp.ac.jp>
2026-04-20 15:57:42 +0900

    Disable summernote HTML sanitizer to fix TypeError

--- config/settings.py
+++ config/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # --- DEBUG設定 ---
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     'my-photo-portfolio.onrender.com',
