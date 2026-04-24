b578ae1087d6a22a30836fb79e10a9bf7f63541b
Shinon <tf23stamada@ec.usp.ac.jp>
2026-04-24 10:03:42 +0900

    Google対策：robots.txtの通行証を追加

--- config/urls.py
+++ config/urls.py
@@ -2,8 +2,16 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import HttpResponse  # 👈 追記1
+
+# 👈 追記2：Googleのロボットに「全部見てOK」と伝える命令
+def robots_txt(request):
+    content = "User-agent: *\nAllow: /"
+    return HttpResponse(content, content_type="text/plain")
 
 urlpatterns = [
+    path('robots.txt', robots_txt),  # 👈 追記3：一番上に追加
+    
     # 🔒 セキュリティ対策済みの秘密の管理画面URL
     path('shino-secret-entry/', admin.site.urls),
 
