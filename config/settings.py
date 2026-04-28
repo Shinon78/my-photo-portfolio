bb013517ddefe63c525e5d5c97abcb8a48262a6c
Shinon <tf23stamada@ec.usp.ac.jp>
2026-04-28 11:47:59 +0900

    setting順番変更

--- config/settings.py
+++ config/settings.py
@@ -32,14 +32,14 @@ CSRF_TRUSTED_ORIGINS = [
 
 # Application definition
 INSTALLED_APPS = [
-    'cloudinary_storage', # Cloudinaryをstaticfilesより先に読み込む
-    'cloudinary',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+    'django.contrib.staticfiles', # 👈 これより【下】に移動させます
+    'cloudinary_storage',         # 👈 ここ！
+    'cloudinary',                 # 👈 ここ！
     'photos',
     'tinymce'
 ]
